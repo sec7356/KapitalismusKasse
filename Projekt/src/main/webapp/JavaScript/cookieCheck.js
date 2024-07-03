9015db0178f90fa0607d5d4c28719e22217e1eb7
sec7356 <168713152+sec7356@users.noreply.github.com>
2024-07-03 15:56:52 +0200

    Cookie Checker bug fix

--- Projekt/src/main/webapp/JavaScript/cookieCheck.js
+++ Projekt/src/main/webapp/JavaScript/cookieCheck.js
@@ -3,59 +3,67 @@
 "use strict";
 
 // Überprüfung der Cookies nach dem Laden der Seite
-        window.onload = checkCookiesEnabled;
-        document.getElementById("reloadButton").addEventListener("click", location.reload());
+window.onload = function() {
+    checkCookiesEnabled();
+
+    var reloadButton = document.getElementById("reloadButton");
+    if (reloadButton) {
+        reloadButton.addEventListener("click", function() {
+            location.reload();
+        });
+    } else {
+        console.error('Element mit der ID "reloadButton" nicht gefunden');
+    }
+};
 
 function checkCookiesEnabled() {
-            // Versuch einen Test-Cookie zu setzen
-            document.cookie = "testcookie=1";
-            const cookiesEnabled = document.cookie.indexOf("testcookie") !== -1;
+    // Versuch einen Test-Cookie zu setzen
+    document.cookie = "testcookie=1";
+    const cookiesEnabled = document.cookie.indexOf("testcookie") !== -1;
 
-            if (cookiesEnabled) {
-                // Cookie löschen
-                document.cookie = "testcookie=1; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
-                if (!getCookie('successShown')) {
-                    showSuccessPopup();
-                    setCookie('successShown', '1', 1); // Cookie setzen für einen Tag
-                }
-            } else {
-                showAlert();
-            }
+    if (cookiesEnabled) {
+        // Cookie löschen
+        document.cookie = "testcookie=1; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+        if (!getCookie('successShown')) {
+            showSuccessPopup();
+            setCookie('successShown', '1', 1); // Cookie setzen für einen Tag
         }
+    } else {
+        showAlert();
+    }
+}
 
-        function showSuccessPopup() {
-            var successPopup = document.getElementById("cookieSuccess");
-            successPopup.style.display = "block";
-            setTimeout(() => {
-                successPopup.style.display = "none";
-            }, 2000);
-        }
+function showSuccessPopup() {
+    var successPopup = document.getElementById("cookieSuccess");
+    successPopup.style.display = "block";
+    setTimeout(() => {
+        successPopup.style.display = "none";
+    }, 2000);
+}
 
-        function showAlert() {
-            alert("Bitte aktivieren Sie Ihre Cookies um fortzufahren");
-            // Seite neu laden nach dem Schließen des Alerts
-            location.reload();
-        }
+function showAlert() {
+    alert("Bitte aktivieren Sie Ihre Cookies um fortzufahren");
+    // Seite neu laden nach dem Schließen des Alerts
+    location.reload();
+}
 
-        function setCookie(name, value, days) {
-            let expires = "";
-            if (days) {
-                const date = new Date();
-                date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-                expires = "; expires=" + date.toUTCString();
-            }
-            document.cookie = name + "=" + (value || "") + expires + "; path=/";
-        }
+function setCookie(name, value, days) {
+    let expires = "";
+    if (days) {
+        const date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "") + expires + "; path=/";
+}
 
-        function getCookie(name) {
-            var nameEQ = name + "=";
-            var ca = document.cookie.split(';');
-            for (let i = 0; i < ca.length; i++) {
-                let c = ca[i];
-                while (c.charAt(0) == ' ') c = c.substring(1, c.length);
-                if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
-            }
-            return null;
-        }
-
-        
+function getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for (let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+    }
+    return null;
+}
